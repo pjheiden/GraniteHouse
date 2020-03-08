3156bd09a146bd168aed6cd34db6ae4223757e2b
pjheiden <46403312+pjheiden@users.noreply.github.com>
2020-03-08 13:10:13 -0400

    shoppingcart

--- GraniteHouse/Areas/Customer/Controllers/ShoppingCartController.cs
+++ GraniteHouse/Areas/Customer/Controllers/ShoppingCartController.cs
@@ -38,7 +38,7 @@ namespace GraniteHouse.Areas.Customer.Controllers
             }
             foreach (var cartItem in lstShoppingCart)
             {
-      //          ShoppingCartVM.Products.Add(_db.Products.Include(m=>m.ProductTypes).Include(m=>m.SpecialTags).FirstOrDefault(m => m.Id == cartItem));
+                //          ShoppingCartVM.Products.Add(_db.Products.Include(m=>m.ProductTypes).Include(m=>m.SpecialTags).FirstOrDefault(m => m.Id == cartItem));
 
                 //ShoppingCartVM.Products.Add(_db.Products
                 //    .Include(m => m.ProductTypes)
@@ -57,17 +57,111 @@ namespace GraniteHouse.Areas.Customer.Controllers
         }
 
 
-        [HttpPost,ActionName("Remove")]
+        // Get
+        public IActionResult ConfirmAppointment(int? id)
+        {
+
+                ShoppingCartVM.Appointments = _db.Appointments.FirstOrDefault(a => a.Id == id);
+                ShoppingCartVM.Products = _db.ProductsSelectedForAppointment.Where(p => p.AppointmentId == id).Select(p => p.Products).Include(p => p.ProductTypes).ToList();
+                return View(ShoppingCartVM);
+
+        }
+
+
+        [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Remove(int itemId, ShoppingCartViewModel sCart)
+        [ActionName("ConfirmAppointment")]
+        public IActionResult ConfirmAppointmentPost()
         {
             List<int> lstShoppingCart = HttpContext.Session.Get<List<int>>("ssShoppingCart");
-            lstShoppingCart.Remove(itemId);
-            var product = ShoppingCartVM.Products.FirstOrDefault(p => p.Id == itemId);
+            ShoppingCartVM.Appointments.AppointmentDate = ShoppingCartVM.Appointments.AppointmentDate
+                .AddHours(ShoppingCartVM.Appointments.AppointmentTime.Hour)
+                .AddMinutes(ShoppingCartVM.Appointments.AppointmentTime.Minute);
 
-            ShoppingCartVM.Products.Remove(product);
-            HttpContext.Session.Set<List<int>>("ssShoppingCart", lstShoppingCart);
-            return View(ShoppingCartVM);
+            Appointments appointments = ShoppingCartVM.Appointments;
+            _db.Appointments.Add(appointments);
+            _db.SaveChanges();
+
+            int appointmentId = appointments.Id;
+
+            foreach (var productId in lstShoppingCart)
+            {
+                _db.ProductsSelectedForAppointment.Add(new ProductsSelectedForAppointment() { AppointmentId = appointmentId, ProductId = productId });
+            }
+            _db.SaveChanges();
+            lstShoppingCart = new List<int>();
+            HttpContext.Session.Set("ssShoppingCart", lstShoppingCart);
+
+            return RedirectToAction("Index");
+            //      return View(ShoppingCartVM);
+        }
+
+
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Index")]
+        public IActionResult IndexPost()
+        {
+            List<int> lstShoppingCart = HttpContext.Session.Get<List<int>>("ssShoppingCart");
+            ShoppingCartVM.Appointments.AppointmentDate = ShoppingCartVM.Appointments.AppointmentDate
+                .AddHours(ShoppingCartVM.Appointments.AppointmentTime.Hour)
+                .AddMinutes(ShoppingCartVM.Appointments.AppointmentTime.Minute);
+
+            Appointments appointments = ShoppingCartVM.Appointments;
+            _db.Appointments.Add(appointments);
+            _db.SaveChanges();
+
+            int appointmentId = appointments.Id;
+
+            foreach (var productId in lstShoppingCart)
+            {
+                _db.ProductsSelectedForAppointment.Add(new ProductsSelectedForAppointment() { AppointmentId = appointmentId, ProductId = productId });
+            }
+            _db.SaveChanges();
+            lstShoppingCart = new List<int>();
+            HttpContext.Session.Set("ssShoppingCart", lstShoppingCart);
+
+            return RedirectToAction("ConfirmAppointment", "ShoppingCart", new { Id = appointmentId });
+            //      return View(ShoppingCartVM);
+        }
+
+
+        //[HttpPost, ActionName("Remove")]
+        //[ValidateAntiForgeryToken]
+        public IActionResult Remove(int id)
+        {
+            List<int> lstShoppingCart = HttpContext.Session.Get<List<int>>("ssShoppingCart");
+
+            if (lstShoppingCart.Count > 0)
+            {
+                if (lstShoppingCart.Contains(id))
+                {
+                    lstShoppingCart.Remove(id);
+
+                    HttpContext.Session.Set<List<int>>("ssShoppingCart", lstShoppingCart);
+                }
+
+
+
+            }
+
+            return RedirectToAction(nameof(Index));
+
+
+
+            //List<int> lstShoppingCart = HttpContext.Session.Get<List<int>>("ssShoppingCart");
+            //if (lstShoppingCart == null)
+            //{
+            //    lstShoppingCart = new List<int>();
+
+            //}
+            //lstShoppingCart.Add(id);
+            //HttpContext.Session.Set("ssShoppingCart", lstShoppingCart);
+
+            //return RedirectToAction(nameof(Index));
 
         }
 
